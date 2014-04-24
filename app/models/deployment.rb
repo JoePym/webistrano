7d57a76b18e951046462518d28c1e935679813b5
Joe Pym <joe@joepym.com>
2014-04-24 16:05:50 -0400

    returned rescue

--- app/models/deployment.rb
+++ app/models/deployment.rb
@@ -56,9 +56,9 @@ class Deployment < ActiveRecord::Base
       stage.lock_with(d)
     end
     true
-  # rescue => e
-  #   RAILS_DEFAULT_LOGGER.debug "DEPLOYMENT: could not fire deployment: #{e.inspect} #{e.backtrace.join("\n")}"
-  #   false
+  rescue => e
+    RAILS_DEFAULT_LOGGER.debug "DEPLOYMENT: could not fire deployment: #{e.inspect} #{e.backtrace.join("\n")}"
+    false
   end
 
   def override_locking?
